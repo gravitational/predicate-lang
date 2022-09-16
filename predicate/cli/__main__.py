9360055a3993dafadff2777c6a67ec5cdf8da9c3
Joel Wejdenstål <jwejdenstal@icloud.com>
2022-09-16 16:41:36 +0200

    add cli parsing lib

--- predicate/cli/__main__.py
+++ predicate/cli/__main__.py
@@ -1,6 +1,12 @@
-def main():
-    print("in main")
+import click
 
+@click.group()
+def main():
+    pass
+
+@main.command()
+def hello():
+    click.echo('Hello World!')
 
 if __name__ == "__main__":
     main()
