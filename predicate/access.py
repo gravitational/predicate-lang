c50785f72ad5532645f97478eb513462ff588654
Joel Wejdenstål <jwejdenstal@icloud.com>
2022-09-16 17:17:58 +0200

    Hack together a harness that can load and fetch classes from policy files

---
+++ predicate/access.py
@@ -0,0 +1,21 @@
+class Policy:
+    policy = Policy(
+        name="access",
+        allow=Rules(
+            Node((Node.login == User.name)),
+        )
+    )
+
+    def test_access():
+        # Alice will be able to login to any machine as herself
+        ret, _ = p.check(Node((Node.login == "alice") & (User.name == "alice")))
+        assert ret is True, "Alice can login with her user to any node"
+
+        # We can verify that a strong invariant holds:
+        # Unless a username is root, a user can not access a server as
+        # root. This creates a problem though, can we deny access as root
+        # altogether?
+        ret, _ = p.check(Node((Node.login == "root") & (User.name != "root")))
+        assert (
+            ret is False
+        ), "This role does not allow access as root unless a user name is root"
--- predicate/cli/__main__.py
+++ predicate/cli/__main__.py
@@ -1,12 +1,27 @@
+from pathlib import Path
+from runpy import run_path
+from types import FunctionType
 import click
+
+from solver.teleport import (
+    Node,
+    Rules,
+    Policy,
+    User
+)
 
 @click.group()
 def main():
     pass
 
 @main.command()
-def hello():
-    click.echo('Hello World!')
+@click.argument('policy-file')
+def test(policy_file):
+    env = {"Policy": Policy, "Rules": Rules, "User": User, "Node": Node}
+    module = run_path(policy_file, env)
+    policyClass = module["Policy"]
+    testMethods = [x for x, y in policyClass.__dict__.items() if type(y) == FunctionType and x.startswith("test_")]
+    click.echo(testMethods)
 
 if __name__ == "__main__":
     main()
